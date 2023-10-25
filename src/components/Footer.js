56b555b7a51db401518113cdf058c75f0c5a4eb9
dreamsalotl <donovanw.walker@gmail.com>
2023-10-25 07:11:21 -0500

    added Footer code

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+function Footer() {
+    return (
+        <footer>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-12'>
+                        <p>&copy; 2023 Donovan Walker</p>
+                        <ul>
+                            <li>
+                                <a href='https://github.com/dreamsalotl' target="_blank" rel="noopener noreferrer">
+                                    Github
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/donovan-walker-b91a21285/" target="_blank" rel="noopener noreferrer">
+                                    LinkedIn
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/dreamsalotl" target="_blank" rel="noopener noreferrer">
+                                    Twitter
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
