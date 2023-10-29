6c38688a276a2413e9ec9f5e5933e092d9b3a096
dreamsalotl <donovanw.walker@gmail.com>
2023-10-29 13:01:05 -0500

    linked AboutMe file to homepage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,43 @@
 function Footer() {
   return (
-    <footer>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <p>&copy; 2023 Donovan Walker</p>
-            <ul>
-              <li>
-                <a
-                  href="https://github.com/dreamsalotl"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Github
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.linkedin.com/in/donovan-walker-b91a21285/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LinkedIn
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://twitter.com/dreamsalotl"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Twitter
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+<footer>
+  <div className="container">
+    <div className="row">
+      <div className="col-md-12">
+        <p>&copy; 2023 Donovan Walker</p>
+        <ul>
+          <li>
+            <a
+              href="https://github.com/dreamsalotl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/donovan-walker-b91a21285/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/dreamsalotl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-twitter"></i>
+            </a>
+          </li>
+        </ul>
       </div>
-    </footer>
+    </div>
+  </div>
+</footer>
   );
 }
 
